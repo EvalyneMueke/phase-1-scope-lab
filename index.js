7095d9cd2fc35077c25a3986ba033556687dffb6
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-11-24 23:38:20 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavouriteCustomer = "Joe";
+
+ function upperCaseCustomerName(){
+    return customerName = "BOB";
+ }
+
+ function setBestCustomer(){
+    return bestCustomer = "not bob";
+   
+ }
+ function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+ }
+ 
+function changeLeastFavoriteCustomer(){
+    return leastFavouriteCustomer = "ted";
+}
